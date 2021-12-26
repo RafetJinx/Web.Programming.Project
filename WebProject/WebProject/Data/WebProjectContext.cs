1fb30b29a6e2610df04dcf5b295624c76d9c20c8
RafetErsoy <rafet.ersoy.cs@gmail.com>
2021-12-26 20:03:49 +0300

    Added "ChampionsController.cs"
    Added "Controllers/ChampionsController.cs"

--- WebProject/WebProject/Data/WebProjectContext.cs
+++ WebProject/WebProject/Data/WebProjectContext.cs
@@ -17,5 +17,7 @@ namespace WebProject.Data
         public DbSet<WebProject.Models.Summoner> Summoner { get; set; }
 
         public DbSet<WebProject.Models.SummonerRankedLeagueDetail> SummonerRankedLeagueDetail { get; set; }
+
+        public DbSet<WebProject.Models.Champion> Champion { get; set; }
     }
 }
