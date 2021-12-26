81c10bd7a11ebf1ee38bb336f7fab5d84a17db0e
RafetErsoy <rafet.ersoy.cs@gmail.com>
2021-12-26 20:16:13 +0300

    Added Migrations
    Added "Migrations/SummonerChampionMasteryCreate.cs"

--- WebProject/WebProject/Migrations/20211226171519_SummonerChampionMasteryCreate.Designer.cs
+++ WebProject/WebProject/Migrations/20211226171519_SummonerChampionMasteryCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebProject.Data;
 
 namespace WebProject.Migrations
 {
     [DbContext(typeof(WebProjectContext))]
-    partial class WebProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20211226171519_SummonerChampionMasteryCreate")]
+    partial class SummonerChampionMasteryCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebProject/WebProject/Migrations/20211226171519_SummonerChampionMasteryCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebProject.Migrations
+{
+    public partial class SummonerChampionMasteryCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SummonerChampionMastery",
+                columns: table => new
+                {
+                    ChampionId = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    ChampionLevel = table.Column<int>(type: "int", nullable: false),
+                    ChampionPoint = table.Column<int>(type: "int", nullable: false),
+                    LastPlayTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ChestGranted = table.Column<bool>(type: "bit", nullable: false),
+                    TokensEarned = table.Column<int>(type: "int", nullable: false),
+                    SummonerId = table.Column<string>(type: "nvarchar(150)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SummonerChampionMastery", x => x.ChampionId);
+                    table.ForeignKey(
+                        name: "FK_SummonerChampionMastery_Champion_ChampionId",
+                        column: x => x.ChampionId,
+                        principalTable: "Champion",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SummonerChampionMastery_Summoner_SummonerId",
+                        column: x => x.SummonerId,
+                        principalTable: "Summoner",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SummonerChampionMastery_SummonerId",
+                table: "SummonerChampionMastery",
+                column: "SummonerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SummonerChampionMastery");
+        }
+    }
+}
