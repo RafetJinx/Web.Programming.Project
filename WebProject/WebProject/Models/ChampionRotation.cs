fdc113ec200507400c2e8ab87d7f99c65f5edf1c
RafetErsoy <rafet.ersoy.cs@gmail.com>
2021-12-26 20:08:23 +0300

    Added "ChampionRotation.cs"
    Added "Models/ChampionRotation.cs"

---
+++ WebProject/WebProject/Models/ChampionRotation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebProject.Models
+{
+    public class ChampionRotation
+    {
+        [Key]
+        [Required]
+        [ForeignKey("Champion")]
+        public string ChampionId { get; set; }
+        public virtual Champion Champion { get; set; }
+    }
+}
